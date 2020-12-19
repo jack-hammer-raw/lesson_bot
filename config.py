03970021f917494cca148dbdd5105ac86c98469d
Jack <jh@raw.ru>
2020-12-19 12:54:22 +0800

    added to schedule students Artem and Sergey

--- config.py
+++ config.py
@@ -7,10 +7,16 @@ users = guests + owners + admins
 
 schedule = """
 Пн: [12:00] Демьян (Python)
+
 Вт: [10:00] Демьян (Python)
+
 Ср: [11:15] Мирон и Саша (Scratch)
+
 Чт: [11:30] Мирон (Python)
+    [16:00] Артем и Сергей (Python)
+
 Пт: [11:00] Мирон и Саша (Scratch)
+
 Сб: [10:00] Демьян (Python)
 """
 
